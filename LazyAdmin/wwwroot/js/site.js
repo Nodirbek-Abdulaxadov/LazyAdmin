8d85ef6bbfc32faeb8abb0ebc9affedb0cd3f7bb
Nodirbek Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2024-05-15 18:10:58 +0500

    Add project files.

---
+++ LazyAdmin/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿var isCollapsed = false;
+
+function toggleButton() {
+    var sidebar = document.getElementById("sidebar");
+    var main = document.getElementById("main");
+    if (isCollapsed) {
+        var links = document.getElementsByClassName("nav-link-text")
+        for (var i = 0; i < links.length; i++) {
+            links[i].style.display = "inline";
+        }
+        sidebar.style.width = "275px";
+        main.style.marginLeft = "275px";
+        isCollapsed = false;
+    } else {
+        sidebar.style.width = "70px";
+        main.style.marginLeft = "70px";
+        var links = document.getElementsByClassName("nav-link-text")
+        for (var i = 0; i < links.length; i++) {
+            links[i].style.display = "none";
+        }
+        isCollapsed = true;
+    }
+}
